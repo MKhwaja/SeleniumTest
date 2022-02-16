1ab60b0fc1aae023420ca9f48229ed7d37499dc7
MKhwaja <53293682+MKhwaja@users.noreply.github.com>
2022-02-15 18:41:31 -0600

    Upload New Repo

---
+++ src/main/AppMain/PageObjects/homePageObjects.java
@@ -0,0 +1,19 @@
+package PageObjects;
+
+import PageComponents.NavigationBar;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class homePageObjects  {
+
+    WebDriver driver;
+    By sectionElement = By.cssSelector("ul.navigation.clearfix");
+
+    public homePageObjects(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public NavigationBar getNavigationBarObj() {
+        return new NavigationBar(driver,sectionElement);
+    }
+}
---
+++ src/test/AppTest/Resources/base.java
@@ -0,0 +1,36 @@
+package Resources;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class base {
+
+    public WebDriver driver;
+
+    public String loadProperties(String property) throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("src/test/AppTest/Resources/data.properties");
+        prop.load(fis);
+        return prop.getProperty(property);
+    }
+
+    public WebDriver intializeDriver() throws IOException {
+        String browserName = loadProperties("browser");
+
+        if(browserName.equalsIgnoreCase("chrome"))
+        {
+            System.setProperty("webdriver.chrome.driver", "src/Drivers/chromedriver");
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        return driver;
+    }
+}
